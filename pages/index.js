140b9dd34e92961750fc251e5088f90d14d0a2ab
Daniel Moraes <36305237+todydanielm3@users.noreply.github.com>
2024-01-19 12:27:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,15 @@ const Home = () => (
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
+          
+          <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M8QBG9KK');</script>
+<!-- End Google Tag Manager -->
+          
             Daniel Moraes
           </Heading>
           <p>
