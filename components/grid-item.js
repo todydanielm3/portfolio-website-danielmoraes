d657006cc6df49d83e467b829aff4b940f2d26ec
Daniel Moraes <36305237+todydanielm3@users.noreply.github.com>
2024-01-26 13:06:19 -0300

    Update grid-item.js

--- components/grid-item.js
+++ components/grid-item.js
@@ -23,12 +23,6 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
 
 export const WorkGridItem = ({ children, id, title, thumbnail }) => (
   <Box w="100%" textAlign="center">
-    <LinkBox
-      as={NextLink}
-      href={`/works/${id}`}
-      scroll={false}
-      cursor="pointer"
-    >
       <Image
         src={thumbnail}
         alt={title}
