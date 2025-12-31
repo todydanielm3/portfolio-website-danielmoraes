d8de1cc461e17143b6d863df0ac1006a1a399d38
Daniel Moraes <36305237+todydanielm3@users.noreply.github.com>
2025-12-31 12:10:11 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ import { GridItem } from '../components/grid-item'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 import thumbYouTube from '../public/images/links/youtube.png'
 //import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import Image from 'next/image'
+import Image from 'next/images'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
